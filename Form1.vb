38873abd673fa00e0c2f1ffe37c3ead6ff69b749
muziiiq <phytonz1807@gmail.com>
2025-06-26 21:52:20 +0800

    label

--- Form1.vb
+++ Form1.vb
@@ -2,5 +2,7 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim num As Integer
         num = InputBox("robux brp")
+
+        Label1 = num
     End Sub
 End Class
